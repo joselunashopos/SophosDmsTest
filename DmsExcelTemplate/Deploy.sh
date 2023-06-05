ec5d0d7bbf40edc4bf9f62df97093414ab7b4dc3
joselunashopos <131683421+joselunashopos@users.noreply.github.com>
2023-06-05 11:27:26 -0500

    Update Deploy.sh

--- DmsExcelTemplate/Deploy.sh
+++ DmsExcelTemplate/Deploy.sh
@@ -1,1 +1,14 @@
+stack_name ="init-dmsexcel-stk"
 
+SCRIPT_DIR=$(dirname "$0")
+template_file="$SCRIPT_DIR/Template1.yml"
+
+parameters="$SCRIPT_DIR/parameters.json"
+
+aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters file://"$parameters" --region "$region" --capabilities CAPABILITY_IAM
+
+aws sts get-caller-identity --query "Account" --output vaccount
+
+bucket = "bucket-dms-excel-$vaccount"
+
+aws s3 mv python.zip s3://"$bucket"
