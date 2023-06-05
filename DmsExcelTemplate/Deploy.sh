3bfb04ab0322a8b9e70f6dd18087f49a381a790f
joselunashopos <131683421+joselunashopos@users.noreply.github.com>
2023-06-05 13:31:44 -0500

    Update Deploy.sh

--- DmsExcelTemplate/Deploy.sh
+++ DmsExcelTemplate/Deploy.sh
@@ -1,14 +1,27 @@
-stack_name ="init-dmsexcel-stk"
+stack_name="init-dmsexcel-stk"
 
 SCRIPT_DIR=$(dirname "$0")
 template_file="$SCRIPT_DIR/Template1.yml"
 
 parameters="$SCRIPT_DIR/parameters.json"
 
+region="us-east-1"
+
 aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters file://"$parameters" --region "$region" --capabilities CAPABILITY_IAM
 
-aws sts get-caller-identity --query "Account" --output vaccount
+sleep 120
 
-bucket = "bucket-dms-excel-$vaccount"
+account_id=$(aws sts get-caller-identity --query "Account" --output text)
 
-aws s3 mv python.zip s3://"$bucket"
+bucket="bucket-dms-excel-$account_id"
+
+#aws s3api put-object --bucket "$bucket" --key libreries/
+
+#sleep 30
+
+aws s3 cp python.zip s3://"$bucket" --output json
+
+template_file="$SCRIPT_DIR/Template2.yml"
+stack_name="dmsexcel-stk"
+
+aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters file://"$parameters" --region "$region" --capabilities CAPABILITY_IAM
