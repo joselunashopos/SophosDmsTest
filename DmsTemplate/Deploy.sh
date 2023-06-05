82bab2eaf95562468dc9732eeef7ea414fd23629
joselunashopos <131683421+joselunashopos@users.noreply.github.com>
2023-06-05 11:14:23 -0500

    Update Deploy.sh

--- DmsTemplate/Deploy.sh
+++ DmsTemplate/Deploy.sh
@@ -7,6 +7,9 @@ stack_name="SKT-tst2"
 #template_file="./Template.yaml"
 
 # Parámetros opcionales de CloudFormation (si es necesario)
+
+aws s3 cp s3://sophos-dev-bucket/auto_files/params.json .
+
 SCRIPT_DIR=$(dirname "$0")
 template_file="$SCRIPT_DIR/template.yml"
 
@@ -16,15 +19,11 @@ pDBSourceName=$(jq -r '.[] | select(.ParameterKey=="pDBSourceName") | .Parameter
 pDBSourceSecretFolder=$(jq -r '.[] | select(.ParameterKey=="pDBSourceSecretFolder") | .ParameterValue' "$SCRIPT_DIR/parameters.json")
 pDMSSchemaFilter=$(jq -r '.[] | select(.ParameterKey=="pDMSSchemaFilter") | .ParameterValue' "$SCRIPT_DIR/parameters.json")
 pDMSTableFilter=$(jq -r '.[] | select(.ParameterKey=="pDMSTableFilter") | .ParameterValue' "$SCRIPT_DIR/parameters.json")
-
-parameters="ParameterKey=pDBSourceEngine,ParameterValue=$pDBSourceEngine \
-ParameterKey=pDBSourcePort,ParameterValue=$pDBSourcePort \
-ParameterKey=pDBSourceName,ParameterValue=$pDBSourceName \
-ParameterKey=pDBSourceSecretFolder,ParameterValue=$pDBSourceSecretFolder \
-ParameterKey=pDMSSchemaFilter,ParameterValue=$pDMSSchemaFilter \
-ParameterKey=pDMSTableFilter,ParameterValue=$pDMSTableFilter"
+parameters="$SCRIPT_DIR/params.json"
 # Región de AWS donde se desplegará el stack
 region="us-east-1"
 
 # Comando para desplegar el stack utilizando la AWS CLI
-aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters "$parameters" --region "$region"
+aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters file://"$parameters" --region "$region" --capabilities CAPABILITY_IAM
+
+rm params.json
