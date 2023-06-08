8714b92c5e9def1160fbc4fb669b79b9412d62bb
joselunashopos <131683421+joselunashopos@users.noreply.github.com>
2023-06-08 15:37:53 -0500

    Update Deploy.sh

--- DmsTemplate/Deploy.sh
+++ DmsTemplate/Deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Nombre del stack de CloudFormation
-stack_name="SKT-tst2"
+stack_name="STK--Dms"
 
 # Ruta al archivo de plantilla de CloudFormation
 #template_file="./Template.yaml"
 
 # Parámetros opcionales de CloudFormation (si es necesario)
 
-aws s3 cp s3://sophos-dev-bucket/auto_files/params.json .
+account_id=$(aws sts get-caller-identity --query "Account" --output text)
+
+bucket="s3://bucket-dms-excel-$account_id/cf_file_out/params.json"
+
+aws s3 cp "$bucket"
 
 SCRIPT_DIR=$(dirname "$0")
 template_file="$SCRIPT_DIR/template.yml"
@@ -21,7 +25,8 @@ pDMSSchemaFilter=$(jq -r '.[] | select(.ParameterKey=="pDMSSchemaFilter") | .Par
 pDMSTableFilter=$(jq -r '.[] | select(.ParameterKey=="pDMSTableFilter") | .ParameterValue' "$SCRIPT_DIR/parameters.json")
 parameters="$SCRIPT_DIR/params.json"
 # Región de AWS donde se desplegará el stack
-region="us-east-1"
+
+region=$(aws configure get region)
 
 # Comando para desplegar el stack utilizando la AWS CLI
 aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters file://"$parameters" --region "$region" --capabilities CAPABILITY_IAM
