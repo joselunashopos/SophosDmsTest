c148c09860fd8e1780423e88cfc7c6b77e2bd4ae
joselunasophos <131683421+joselunasophos@users.noreply.github.com>
2023-06-09 11:49:55 -0500

    Update Deploy.sh

--- DmsTask/Deploy.sh
+++ DmsTask/Deploy.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# Nombre del stack de CloudFormation
+stack_name="Dms--Task"
+
+# Ruta al archivo de plantilla de CloudFormation
+#template_file="./Template.yaml"
+
+# Parámetros opcionales de CloudFormation (si es necesario)
+
+account_id=$(aws sts get-caller-identity --query "Account" --output text)
+
+bucket="s3://bucket-dms-excel-$account_id/cf_file_out/taskparams.json"
+
+aws s3 cp "$bucket"
+
+SCRIPT_DIR=$(dirname "$0")
+template_file="$SCRIPT_DIR/template.yml"
+parameters="$SCRIPT_DIR/taskparams.json"
+# Región de AWS donde se desplegará el stack
+
+region=$(aws configure get region)
+
+# Comando para desplegar el stack utilizando la AWS CLI
+aws cloudformation create-stack --stack-name "$stack_name" --template-body file://"$template_file" --parameters file://"$parameters" --region "$region" --capabilities CAPABILITY_IAM
+
+rm taskparams.json
